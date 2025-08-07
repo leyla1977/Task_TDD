e510dfdc2ade5034318d117e923ccc46958adf4e
elvira egorova <leylaazbuhan@gmail.com>
2025-08-08 00:23:58 +0300

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,13 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> contacts = new HashMap<>();
+
+    // Заглушка add
+    public int add(String name, String number) {
+        return 0;
+    }
 }
--- src/main/java/ru/netology/PhoneBookTest.java
+++ src/main/java/ru/netology/PhoneBookTest.java
@@ -1,4 +1,23 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PhoneBookTest {
+    @Test
+    void testAddNewContact() {
+        PhoneBook phoneBook = new PhoneBook();
+        int size = phoneBook.add("Alice", "9101234567");
+        assertEquals(1, size);
+    }
+
+    @Test
+    void testAddDuplicateContact() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Bob", "9259876543");
+        int size = phoneBook.add("Bob", "9259876543"); // Дубликат по имени
+        assertEquals(1, size); // Размер не увеличивается
+    }
+
 }
